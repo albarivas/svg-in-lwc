91753173938ad2ddef9be5d8bd4b5622942fa5f6
Alba Rivas <alba.azconarivas@gmail.com>
2020-04-24 17:33:00 +0200

    Add static resource in SVG use case

---
+++ force-app/main/default/lwc/salesforceAwardsInvitation/salesforceAwardsInvitation.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import SVG_LOGO from '@salesforce/resourceUrl/salesforceLogo';
+
+export default class SalesforceAwardsInvitation extends LightningElement {
+    svgURL = `${SVG_LOGO}#sflogo`
+}
